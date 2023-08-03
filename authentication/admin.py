9eb8649da3b2803a1499e93a3da69972c8556cfa
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-03 14:11:29 +0700

    feat: register manual & google user to admin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from authentication.models import ( 
+    ManualUser,
+    GoogleUser
+)
 
-# Register your models here.
+@admin.register(ManualUser)
+class ManualUser(admin.ModelAdmin):
+    fields = []
+    list_display = ['username', 'email', 'password', 'first_name', 'last_name', 'mobile_number', 'photo_url']
+
+@admin.register(GoogleUser)
+class GoogleUser(admin.ModelAdmin):
+    fields = []
+    list_display = ['google_uid', 'email', 'name', 'mobile_number', 'photo_url']

fdc703d2479b52162c32aa59029b75711fafc5c6
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-03 14:15:22 +0700

    feat: api view for manual & google user in auth service

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from authentication.models import (
+    ManualUser,
+    GoogleUser
+)
+from authentication.serializers import ( 
+    ManualUserSerializer,
+    GoogleUserSerializer
+)
 
-# Create your views here.
+class ManualUserAPIView(generics.ListAPIView):
+    serializer_class = ManualUserSerializer
+
+    def get_queryset(self):
+        manual_users = ManualUser.objects.all()
+        return manual_users
+
+    def post(self, request, *args, **kwargs):
+        serializer = ManualUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+class GoogleUserAPIView(generics.ListAPIView):
+    serializer_class = GoogleUserSerializer
+
+    def get_queryset(self):
+        google_users = GoogleUser.objects.all()
+        return google_users
+
+    def post(self, request, *args, **kwargs):
+        serializer = GoogleUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)

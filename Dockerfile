619cd8d7f5e5a92ef566388904b1b0bdc04c55a1
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-08-16 02:50:35 +0700

    setup: change to google app engine

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.10.6
-
-ENV PYTHONUNBUFFERED 1
-RUN mkdir /safewalk
-WORKDIR /safewalk
-ADD . /safewalk/
-RUN pip install -r requirements.txt
--- safewalk/settings.py
+++ safewalk/settings.py
@@ -13,21 +13,29 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 PRODUCTION = os.getenv('DATABASE_URL') is not None
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9zeg2%ik)*135-mykq6z5j30*00mfom*e9rbq^***l64o%!7=-"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','http://*.127.0.0.1']
@@ -95,8 +103,11 @@ WSGI_APPLICATION = "safewalk.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "/cloudsql/safewalk-testing-ae:asia-southeast2:safewalk-testing-ae",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "NAME": "belerion-db"
     }
 }
 

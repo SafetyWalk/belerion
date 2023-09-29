01eccf7357b317e39a012f1103e308b2cfda0775
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-09-29 18:34:14 +0700

    feat: change serializers implementation

--- maps/serializers.py
+++ maps/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
-from maps.models import (
-    Coordinate,
-    Polygon
-)
-
-class CoordinateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coordinate
-        fields = ['lat', 'lng']
+from maps.models import Polygon, Coordinate
 
 class PolygonSerializer(serializers.ModelSerializer):
-    coordinates = CoordinateSerializer(many=True, read_only=True)
+    coordinates = serializers.SerializerMethodField()
 
     class Meta:
         model = Polygon
         fields = ['id', 'coordinates']
+
+    def get_coordinates(self, obj):
+        # Mengambil semua koordinat untuk polygon objek saat ini
+        coordinates = obj.coordinates.all()
+        
+        # Mengubah setiap koordinat menjadi objek {'lat': lat_value, 'lng': lng_value}
+        coordinates_data = [{'lat': coord.lat, 'lng': coord.lng} for coord in coordinates]
+        
+        return coordinates_data
